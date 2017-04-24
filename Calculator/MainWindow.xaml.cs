9f6ddf073804588002db79a0339ee11a8a110946
Benjamin Rosenkranz <benjamin.rosenkranz@gmail.com>
2017-04-24 19:47:20 +0200

    Ziffern hinzugefügt sowie die operatoren. Ausserdem ist alles "skaliert"

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -24,5 +24,91 @@ namespace Calculator
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //7
+            textbox2.Inlines.Add("7");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //8
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            //9
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            //=
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            //4
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            //5
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            //6
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            //-
+        }
+
+        private void Button_Click_9(object sender, RoutedEventArgs e)
+        {
+            //1
+        }
+
+        private void Button_Click_10(object sender, RoutedEventArgs e)
+        {
+            //2
+        }
+
+        private void Button_Click_11(object sender, RoutedEventArgs e)
+        {
+            //3
+        }
+
+        private void Button_Click_12(object sender, RoutedEventArgs e)
+        {
+            //X
+        }
+
+        private void Button_Click_13(object sender, RoutedEventArgs e)
+        {
+            //0
+        }
+
+        private void Button_Click_14(object sender, RoutedEventArgs e)
+        {
+            //,
+        }
+
+        private void Button_Click_15(object sender, RoutedEventArgs e)
+        {
+            ///
+        }
     }
 }
